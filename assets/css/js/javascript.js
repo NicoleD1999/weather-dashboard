c37924be7cc7d280ca7b444551ca1449ff27520b
NicoleD1999 <nmd1018@yahoo.com>
2022-04-02 14:49:09 -0500

    rearranged layout of html and added javascript to retrieve API from Open Weather

--- assets/css/js/javascript.js
+++ assets/css/js/javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+
+
+function getApi() {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        
+      });
+}
+getApi();
